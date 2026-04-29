88ca2098fd31a7bf2ae5fca5c37d5b9e6268539c
Vandal <bjpowerll12967@gmail.com>
2026-04-29 14:37:07 +0900

    https://rinthel.github.io/opengl_course/04_graphics_pipeline_and_shader_01#/28

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,35 @@
-﻿#include <GLFW/glfw3.h>
+﻿// clang-format off
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+// clang-format on
 #include <spdlog/spdlog.h>
+
+void OnFramebufferSizeChange( GLFWwindow *window, int width, int height )
+{
+    SPDLOG_INFO( "framebuffer size changed: ({} x {})", width, height );
+    glViewport( 0, 0, width, height );
+}
+
+void OnKeyEvent( GLFWwindow *window, int key, int scancode, int action, int mods )
+{
+    SPDLOG_INFO( "key: {}, scancode: {}, action: {}, mods: {}{}{}", key, scancode,
+                 action == GLFW_PRESS     ? "Pressed"
+                 : action == GLFW_RELEASE ? "Released"
+                 : action == GLFW_REPEAT  ? "Repeat"
+                                          : "Unknown",
+                 mods & GLFW_MOD_CONTROL ? "C" : "-", mods & GLFW_MOD_SHIFT ? "S" : "-",
+                 mods & GLFW_MOD_ALT ? "A" : "-" );
+    if ( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS )
+    {
+        glfwSetWindowShouldClose( window, true );
+    }
+}
+
+void Render()
+{
+    glClearColor( 0.1f, 0.2f, 0.3f, 0.0f );
+    glClear( GL_COLOR_BUFFER_BIT );
+}
 
 int main( int argc, const char **argv )
 {
@@ -16,20 +46,43 @@ int main( int argc, const char **argv )
         return -1;
     }
 
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+    glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+
     // glfw 윈도우 생성, 실패하면 에러 출력후 종료
     SPDLOG_INFO( "Create glfw window" );
     auto window = glfwCreateWindow( WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr );
     if ( !window )
     {
-        SPDLOG_ERROR( "failed to create glfw window" );
+        SPDLOG_ERROR( "Failed to create glfw window" );
         glfwTerminate();
         return -1;
     }
+
+    glfwMakeContextCurrent( window );
+
+    if ( !gladLoadGLLoader( reinterpret_cast<GLADloadproc>( glfwGetProcAddress ) ) )
+    {
+        SPDLOG_ERROR( "failed to initialize glad" );
+        glfwTerminate();
+        return -1;
+    }
+    auto glVersion = reinterpret_cast<const char *>( glGetString( GL_VERSION ) );
+    SPDLOG_INFO( "OpenGL context version: {}", glVersion );
+
+    // 정의한 콜백을 윈도우에 등록
+    // 윈도우 생성 직후에는 프레임버퍼 변경 이벤트가 발생하지 않으므로 첫 호출은 수동으로 함
+    OnFramebufferSizeChange( window, WINDOW_WIDTH, WINDOW_HEIGHT );
+    glfwSetFramebufferSizeCallback( window, OnFramebufferSizeChange );
+    glfwSetKeyCallback( window, OnKeyEvent );
 
     // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO( "Start main loop" );
     while ( !glfwWindowShouldClose( window ) )
     {
+        Render();
+        glfwSwapBuffers( window );
         glfwPollEvents();
     }
 
